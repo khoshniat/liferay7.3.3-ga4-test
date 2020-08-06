e42597468e74b9eed79c122f4d84a11466ff0bc8
khoshniat hossein <khoshniat_hossein@yahoo.com>
2020-08-06 09:40:56 +0430

    initial commit

---
+++ test-service/src/main/resources/META-INF/sql/indexes.sql
@@ -0,0 +1,3 @@
+create index IX_46C1E5EB on TEST_Foo (field2);
+create index IX_1A98D133 on TEST_Foo (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_A7A0875 on TEST_Foo (uuid_[$COLUMN_LENGTH:75$], groupId);
---
+++ test-web/src/main/java/ir/sain/test/constants/TestPortletKeys.java
@@ -0,0 +1,11 @@
+package ir.sain.test.constants;
+
+/**
+ * @author ParsFarhangAfzar
+ */
+public class TestPortletKeys {
+
+	public static final String TEST =
+		"ir_sain_test_TestPortlet";
+
+}
